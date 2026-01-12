beffc428f91e58132b1b11af45f22f58866bca9b
icssoa <615206459@qq.com>
2026-01-12 13:55:22 +0800

    1

---
+++ .vitepress/dist/demo/assets/file.Dq13rScV.js
@@ -0,0 +1,1 @@
+import{aS as e}from"./index-BS3zmeYS.js";function a(a){return new Promise((n=>{var o;n(URL.createObjectURL(e(null!==(o=null==a?void 0:a.toDataURL("image/png",1))&&void 0!==o?o:"")))}))}export{a as c};
